792895aee7610c93238beafb12d61676f8db9089
manudevelopia <manu@developia.info>
2022-10-04 21:34:31 +0200

    add new field to model

--- pkg/menu/menu.go
+++ pkg/menu/menu.go
@@ -3,8 +3,9 @@ package menu
 import "time"
 
 type Menu struct {
-	ID        uint      `json:"id"`
-	Name      string    `json:"name"`
-	CreatedOn time.Time `json:"createdOn"`
-	UpdatedOn time.Time `json:"updatedOn"`
+	ID          uint      `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	CreatedOn   time.Time `json:"createdOn"`
+	UpdatedOn   time.Time `json:"updatedOn"`
 }
